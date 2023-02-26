045128ed71df205acfd8341d0dc8bb42d93ef0e2
Rocky Haque <99040331+rockyhaque@users.noreply.github.com>
2023-02-26 21:39:56 +0600

    Create 1619A-squareString.cpp

---
+++ 1619A-squareString.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        if(s.size()%2 != 0)
+            cout<<"NO\n";
+        else
+        {
+            string s1;
+            string s2;
+
+            for(int i = 0; i < s.size()/2; i++)
+            {
+                s1.push_back(s[i]);
+            }
+            for(int i = s.size()/2; i < s.size(); i++)
+            {
+                s2.push_back(s[i]);
+            }
+
+            if(s1 == s2)
+                cout<<"YES"<<"\n";
+            else
+                cout<<"NO"<<"\n";
+        }
+
+    }
+    return 0;
+}
